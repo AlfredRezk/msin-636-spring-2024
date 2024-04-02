2a2c084ebdf4f23f5eeed7868da541174e61ea3b
Alfred Rezk <73665419+AlfredRezk@users.noreply.github.com>
2024-04-01 23:49:42 -0400

    Add files via upload

---
+++ amazon-v7/middleware/locals.js
@@ -0,0 +1,7 @@
+module.exports = (req, res, next)=>{
+        res.locals.isAuthenticated = req.session.isLoggedIn;
+        res.locals.errorMessage = req.flash('error');
+        res.locals.successMessage = req.flash('success');
+        res.locals.cartQty = req.session?.user?.cart?.totalQty; 
+        next();
+    }
